eae228a40c21c0f6b91971a86c77584d9cf71d58
Ivan Chernyshov <ivan-chernyshoff@yandex.ru>
2025-07-22 09:47:33 +0300

    Changes IR processing to take zip as input

--- scripts/process_ir_spectra.py
+++ scripts/process_ir_spectra.py
@@ -2,7 +2,7 @@
 
 #%% Imports
 
-import re, os, argparse
+import re, os, zipfile, tempfile, argparse
 
 import pandas as pd
 
@@ -13,32 +13,56 @@ from tqdm import tqdm
 
 #%% Functions
 
-
-
-
-def process_ir_spectra(dir_jdx: str) -> pd.core.frame.DataFrame | None:
-    '''Reads JDX-formatted IR spectra from the given directory,
-    processes them, and returns tabular-formatted info
+def process_ir_spectra(path_zip: str, path_comp: str, path_out: str) -> None:
+    '''Extracts meta-info on IR spectra and saves it to csv-file
     
     Arguments:
-        dir_jdx (str): path to the directory containing IR spectra
-    
-    Returns:
-        pd.core.frame.DataFrame: table containing meta-info on IR spectra
+        path_zip (str): zip-file containing raw IR spectra
+        path_comp (str): path to compounds.csv file
+        path_out (str): output csv file
     
     '''
-    df = []
-    for filename in tqdm(os.listdir(dir_jdx)):
-        cID, _, sID = filename.replace('.jdx', '').split('_')
-        filepath = os.path.join(dir_jdx, filename)
-        X = jcamp_readfile(filepath)
-        for key in ('x', 'y'):
-            X.pop(key)
-        X = {'nist_compound_id': cID, 'nist_ir_id': sID, **X}
-        df.append(X)
-    df = pd.DataFrame(df)
+    # prepare names and inchis
+    main = pd.read_csv(path_comp, low_memory=False)
+    main = main[['ID', 'name', 'inchi']]
+    main = main.set_index('ID')
     
-    return df
+    # output
+    data = []
+    
+    # prepare ZIP
+    with zipfile.ZipFile(path_zip, 'r') as zipf:
+        # iterate files
+        for f in tqdm(zipf.filelist, total = len(zipf.filelist)):
+            if f.is_dir():
+                continue
+            cID, _, sID = os.path.basename(f.filename).replace('.jdx', '').split('_')
+            try:
+                # read file
+                with tempfile.TemporaryFile(mode='wb', delete=False) as jdx:
+                    text = zipf.read(f)
+                    jdx.write(text)
+                    jdx.close()
+                    X = jcamp_readfile(jdx.name)
+                    os.remove(jdx.name)
+                # form row
+                for key in ('x', 'y'):
+                    X.pop(key)
+                X = {'cID': cID, 'name': main.loc[cID, 'name'],
+                     'inchi': main.loc[cID, 'inchi'], 'sID': sID, **X}
+                data.append(X)
+            except (KeyboardInterrupt, SystemExit):
+                raise
+            except Exception:
+                tqdm.write(f'Cannot process {cID} compound, spectrum #{sID}')
+                continue
+    
+    # save csv
+    df = pd.DataFrame(data)
+    df = df.sort_values(['cID', 'sID'])
+    df.to_csv(path_out, index=None)
+    
+    return
 
 
 #%% Main functions
@@ -50,9 +74,10 @@ def get_arguments() -> argparse.Namespace:
         argparse.Namespace: CLI arguments
     
     '''
-    parser = argparse.ArgumentParser(description = 'Extracts meta-info from JDX-formatted IR spectra and saves it as CSV-file')
-    parser.add_argument('dir_jdx', help = 'Directory containing JDX-formatted IR spectra')
-    parser.add_argument('path_out', help = 'Output CSV file')
+    parser = argparse.ArgumentParser(description = 'Extracts annotation from raw IR spectra')
+    parser.add_argument('path_zip', help = 'zip-file containing raw IR spectra')
+    parser.add_argument('path_comp', help = 'compounds.csv')
+    parser.add_argument('path_out', help = 'output CSV-file')
     args = parser.parse_args()
     
     return args
@@ -66,11 +91,14 @@ def check_arguments(args: argparse.Namespace) -> None:
     
     '''
     # check zip
-    if not os.path.isdir(args.dir_jdx):
-        raise ValueError(f'Given dir_jdx directory does not exist: {args.dir_jdx}')
+    if not os.path.exists(args.path_zip):
+        raise ValueError(f'Given path_zip argument does not exist: {args.path_zip}')
+    # check compounds.csv
+    if not os.path.exists(args.path_comp):
+        raise ValueError(f'Given path_comp argument does not exist: {args.path_comp}')
     # check output path
     dir_out = os.path.dirname(args.path_out)
-    if not os.path.isdir(dir_out):
+    if dir_out and not os.path.isdir(dir_out):
         raise ValueError(f'Output directory does not exist: {args.path_out}')
     
     return
@@ -85,9 +113,7 @@ def main() -> None:
     
     # process spectra
     print('\nProcessing IR spectra ...')
-    df = process_ir_spectra(args.dir_jdx)
-    if df is not None:
-        df.to_csv(args.path_out, index=None)
+    process_ir_spectra(args.path_zip, args.path_comp, args.path_out)
     print()
     
     return
